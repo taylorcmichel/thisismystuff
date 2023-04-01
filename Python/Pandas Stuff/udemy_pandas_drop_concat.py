ea089ec125ae0f5343b121ca19b78049ce44088a
taylorcmichel <68913894+taylorcmichel@users.noreply.github.com>
2023-04-01 18:28:16 -0400

    Add files via upload

---
+++ Python/Pandas Stuff/udemy_pandas_drop_concat.py
@@ -0,0 +1,52 @@
+# This script operates on a 'Udemy Courses Data 2023' dataset
+# from Kaggle that can be downloaded here:
+#
+# https://www.kaggle.com/datasets/ankushbisht005/udemy-courses-data-2023
+#
+# The intent of this script is to:
+# 1. Work off of the 'instructors.csv' file in the repository
+# 2. Deconstruct the 'Instructors' data table
+# 3. Create an additional table with pertinent data from the first
+# 4. Delete or 'drop' the columns containing references to images
+# 5. Reconstruct the original table using the two 'new' tables
+# 
+# Yes, this is a much more complicated way to get same result that could
+# be obtained by simply dropping columns from the original table, but the
+# purpose of this script is to help demonstrate my ability to use the
+# Pandas library.
+
+import pandas as pd
+
+#Mac filepaths
+corepath = '/Users/ifhpclothing/Documents/Python_Scripts/'
+filepath = corepath + 'Kaggle Udemy/'
+
+#Set dataframe option to print all columns upon printing
+#Personal preference, not required
+pd.set_option('display.max_columns', None)
+
+#Read raw CSV into dataframe
+df = pd.read_csv(filepath + 'instructors.csv')
+
+#Create lists from columns that will be moved to separate table
+instructor_job_title = df['job_title'].tolist()
+instructor_initials = df['initials'].tolist()
+instructor_url = df['url'].tolist()
+
+#Create new dataframe/table using lists created just above
+ndf = pd.DataFrame({'job_title': instructor_job_title,
+					'initials': instructor_initials,
+					'url': instructor_url})
+
+#Drop above columns plus those with references to images from original
+#dataframe/table
+df.drop(df.columns[[5, 6, 7, 8, 9]], axis=1, inplace=True)
+
+#Write both dataframes/tables to new CSVs
+df.to_csv(filepath + 'instructors_left.csv')
+ndf.to_csv(filepath + 'instructors_right.csv')
+
+result = pd.concat([df, ndf], axis=1, join='inner')
+result.to_csv(filepath + 'instructors_result_concat.csv')
+
+
---
+++ Python/Pandas Stuff/udemy_pandas_simple_drop.py
@@ -0,0 +1,38 @@
+# This script operates on a 'Udemy Courses Data 2023' dataset
+# from Kaggle that can be downloaded here:
+#
+# https://www.kaggle.com/datasets/ankushbisht005/udemy-courses-data-2023
+#
+# The intent of this script is to:
+# 1. Work off of the 'instructors.csv' file in the repository
+# 2. Delete or 'drop' unnecessary or unwanted columns
+# 3. Double check to ensure the remaining columns are those we want
+# 
+# This method is much more efficient than that depicted in 'udemy_pandas'
+# in terms of crafting the end product that we desire from the table
+# that we are given.
+
+import pandas as pd
+
+#Mac filepaths
+corepath = '/Users/ifhpclothing/Documents/Python_Scripts/'
+filepath = corepath + 'Kaggle Udemy/'
+
+#Set dataframe option to print all columns upon printing
+#Personal preference, not required
+pd.set_option('display.max_columns', None)
+
+#Read raw CSV into dataframe
+df = pd.read_csv(filepath + 'instructors.csv')
+
+#Drop columns indexed at 6 and 7
+df.drop(df.columns[[6,7]], axis=1, inplace=True)
+
+#Print a list of the current column headers after the drop to ensure the
+#drop affected the correct columns
+print(list(df.columns))
+
+#Write the amended dataframe to a new CSV
+df.to_csv(filepath + 'instructors_result_drop.csv')
+
+
