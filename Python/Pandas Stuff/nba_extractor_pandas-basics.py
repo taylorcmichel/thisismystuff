11adcf376497f8bb0a1f56116d121fe249a35e66
taylorcmichel <68913894+taylorcmichel@users.noreply.github.com>
2023-04-08 10:38:59 -0400

    Update nba_extractor_pandas-basics.py

--- Python/Pandas Stuff/nba_extractor_pandas-basics.py
+++ Python/Pandas Stuff/nba_extractor_pandas-basics.py
@@ -19,8 +19,8 @@
 
 import pandas as pd
 
-corepath = '/Users/ifhpclothing/Documents'
-filepath = corepath + '/Python_Scripts/NBA Separated CSVs'
+corepath = '*'
+filepath = corepath + '*'
 
 #Read the raw CSV into the main dataframe
 data = pd.read_csv('nba_data_processed.csv')
