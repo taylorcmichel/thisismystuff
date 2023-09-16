21939550798bc54c10b694beb53c1490cd2e4d91
taylorcmichel <68913894+taylorcmichel@users.noreply.github.com>
2023-09-16 14:22:23 -0400

    Create Region_Table.sql

---
+++ SQL/MySQL/Big_Bob's_Boat_Sales/Region_Table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Region_Table (
+	Region_ID 	varchar(2)	  NOT NULL,
+	Region 		  varchar(10)	  NOT NULL,
+  PRIMARY KEY (Region_ID)
+);
+
+INSERT INTO Region_Table (
+	Region_ID,
+	Region
+)
+VALUES
+	('R1', 'West'		  ),
+	('R2', 'Midwest'	),
+	('R3', 'South'		),
+	('R4', 'Northeast');
+
+SELECT *
+FROM Region_Table
