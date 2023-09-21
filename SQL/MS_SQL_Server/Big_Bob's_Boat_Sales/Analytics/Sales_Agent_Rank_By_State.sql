2731ea32c57208237e9963198cf0c3dd41d59f42
taylorcmichel <68913894+taylorcmichel@users.noreply.github.com>
2023-09-21 16:52:26 -0400

    Create Sales_Agent_Rank_By_State.sql

---
+++ SQL/MS_SQL_Server/Big_Bob's_Boat_Sales/Analytics/Sales_Agent_Rank_By_State.sql
@@ -0,0 +1,20 @@
+SELECT
+	DISTINCT(Sales_Agent_Name),
+	Qty_Sold,
+	Principle_State,
+	DENSE_RANK() OVER(PARTITION BY Principle_State ORDER BY Qty_Sold DESC) AS Qty_Sold_Rank
+FROM Orders_Table a
+INNER JOIN Products_Table b
+ON (
+	a.Product_Code = b.Product_Code
+)
+INNER JOIN Sales_Agent_Table c
+ON (
+	a.Sales_Agent_ID = c.Sales_Agent_ID
+)
+INNER JOIN States_Table d
+ON (
+	a.State_ID = d.Location_ID
+)
+WHERE Principle_State = 'Connecticut'
+ORDER BY Qty_Sold DESC;
